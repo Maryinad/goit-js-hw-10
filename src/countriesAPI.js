aeeee3adfedd34e21b1a38e67e6dc6e774bd3292
Maryna Ilinova <80976268+Maryinad@users.noreply.github.com>
2023-01-15 21:50:03 +0200

    Added changes

--- src/countriesAPI.js
+++ src/countriesAPI.js
@@ -1,26 +1,34 @@
 // export class CountryAPI {
-//   static searchCountry() {
-//     const url = `https://restcountries.com/v3.1/?name/${query}`;
-//     // const params = new URLSearchParams({
-//     //   name: query,
-//     // });
-//     fetch(url, options);
+//   static searchCountry(name) {
+//     const params = new URLSearchParams({
+//       name: query,
+//       capital: '',
+//       population: '',
+//       flags: '',
+//       languages: [],
+//     });
+
+//     const url = `https://restcountries.com/v3.1/?name/${query}${params}`;
+//     return fetch(url, options).then(response => response.json);
 //   }
 // }
 
 const BASE_URL = 'https://restcountries.com';
 
 export const fetchCountries = query => {
-  return fetch(`${BASE_URL}/v3.1/name/${query}`).then(res => {
+  return fetch(`${BASE_URL}/v3.1/name/${query}?fields=name,capital,population,flags,languages
+`).then(res => {
     console.log(res);
     if (!res.ok) {
       throw res.status;
+      //   throw new Error(res.status); //?
     }
     return res.json();
   });
 };
 
-fetchCountries('Ukraine').then(data => console.log(data));
+// fetchCountries('Ukraine').then(data => console.log(data));
 
 // ${BASE_URL}/v3.1/name/${query}
 // fetch(`${BASE_URL}/v2/all?fields=${query},capital,population`);
+// ?fields=name,capital,population,flags,languages
--- src/index.js
+++ src/index.js
@@ -1,37 +1,83 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from './countriesAPI';
+import { CountryAPI, fetchCountries } from './countriesAPI';
 
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
   searchEl: document.querySelector('#search-box'),
   contriesListEl: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
-refs.searchEl.addEventListener('input', debounce(onSearchElInput, 300));
+refs.searchEl.addEventListener(
+  'input',
+  debounce(onSearchElInput, DEBOUNCE_DELAY)
+);
 
 function onSearchElInput(e) {
   const query = e.target.value.trim();
   // console.log(query);
   //   console.log('hello');
 
+  // const result = CountryAPI.searchCountry(query);  ??
+  // console.log(result);
   fetchCountries(query)
     .then(data => {
+      if (query == '') {
+        refs.contriesListEl.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        console.log("Please, enter country's name");
+      } else if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        refs.contriesListEl.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+      } else if (data.length > 2 && data.length < 10) {
+        refs.countryInfo.innerHTML = '';
+        renderCountries(data);
+      } else {
+        refs.contriesListEl.innerHTML = '';
+        renderCountry(data);
+      }
       console.log(data);
-      renderCountry(data);
+      // renderCountries(data);
     })
     .catch(err => {
-      console.log(err);
+      // if (err.message === 404) / ?
+      if (err === 404) {
+        Notify.failure('Oops, there is no country with that name');
+      }
+
+      refs.contriesListEl.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
     });
 }
 
-function renderCountry(data) {
-  const markup = `<li class="country-card-item card-name">${data[0].flag} <b>${data[0].name.official}</b></li>
-    <li class="country-card-item"><b>Capital: </b>${data[0].capital}</li>
-    <li class="country-card-item"><b>Population: </b>${data[0].population}</li>
-    <li class="country-card-item"><b>Languages: </b>${data[0].languages}</li>
-  `;
+function renderCountries(data) {
+  const markup = data
+    .map(el => {
+      return `<li class = "country-item"><img src="${el.flags.png}" alt="" width = 50 class="country-img" ><b>${el.name.common}</b></li>`;
+    })
+    .join('');
+
   refs.contriesListEl.innerHTML = markup;
 }
+
+function renderCountry(data) {
+  const values = Object.values(data[0].languages);
+  console.log(values);
+  const markup = `<h1 class="country-info-title">
+      <img src= "${data[0].flags.png}" alt="" width = 50>
+       ${data[0].name.official}</h1>
+       <ul class="country-info-list">
+    <li class="country-info-item"><b>Capital: </b>${data[0].capital}</li>
+    <li class="country-info-item"><b>Population: </b>${data[0].population}</li>
+    <li class="country-info-item"><b>Languages: </b>${values.join(', ')}</li>
+    </ul>
+  `;
+  refs.countryInfo.innerHTML = markup;
+}
+// netherland
